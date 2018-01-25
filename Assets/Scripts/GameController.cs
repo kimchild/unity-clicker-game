5c62b377ebbfc507ea924f131d0f9ce2c3ff8a12
allcreation <allcreation@nate.com>
2018-01-25 13:22:29 +0900

    clicker game 1seconds gold plus

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text TextGold;
+
+
+	// Use this for initialization
+	void Start () {
+		TextGold.text = DataController.Instance.Gold.ToString();
+		StartCoroutine (StartCollectGold ());
+	}
+
+	//coroutine
+	IEnumerator StartCollectGold(){
+	    
+		while (true) {
+			
+			//1seconds sleep
+			yield return new WaitForSecondsRealtime (1f);
+			DataController.Instance.Gold += DataController.Instance.GoldPerSec;
+			TextGold.text = DataController.Instance.Gold.ToString ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
