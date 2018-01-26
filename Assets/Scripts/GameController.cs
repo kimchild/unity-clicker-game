59b3f70f6de35f7a9d71e2ff76ef307c051dfe7d
allcreation <allcreation@nate.com>
2018-01-26 13:25:44 +0900

    효과음, 사운드, 이펙트 추가

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
 	public Text TextGold;
-
+	public Camera MainCamera;
+	public GameObject EffectSpark;
+	public AudioClip SFXClick;
 
 	// Use this for initialization
 	void Start () {
@@ -28,6 +30,32 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		//left click
+		if (Input.GetMouseButtonDown (0)) {
+
+			//마우스 클릭시 골드증가
+			DataController.Instance.Gold += DataController.Instance.GoldPerSec;
+			TextGold.text = DataController.Instance.Gold.ToString ();
+
+			//클릭한 위치에 카메라 이펙트 효과
+			Ray ray = MainCamera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 100f)) {
+				Debug.Log (hit.point);
+				Debug.DrawLine (ray.origin, hit.point, Color.red);
+				Instantiate (EffectSpark, hit.point, EffectSpark.transform.rotation);
+
+				//클릭 사운드
+				MainCamera.gameObject.GetComponent<AudioSource> ().PlayOneShot(SFXClick);
+			}
+		}
+
 	}
+
+
+	public void UpgradeCollectGold(){
+	    
+	}
+
 }
